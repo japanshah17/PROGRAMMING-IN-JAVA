3a61a07f3cf53ed68266cb126c3db181b43796b4
Japan Shah <64722088+japanshah17@users.noreply.github.com>
2021-06-27 17:03:47 +0530

    Delete Yeah.java

--- Yeah.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class Yeah{
-public static void main(String [] args){
-Scanner myobj = new Scanner(System.in);
-System.out.print("enter name: ");
-String name = myobj.nextLine();
-System.out.print("name is " + name);
-}
-}
