232752215793c98a60b6a5a77f2339210baee5b3
Japan Shah <64722088+japanshah17@users.noreply.github.com>
2021-06-27 17:04:54 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Input{
+public static void main(String [] args){
+Scanner myobj = new Scanner(System.in);
+System.out.print("enter name: ");
+String name = myobj.nextLine();
+System.out.print("name is " + name);
+}
+}
