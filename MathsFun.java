d715a503b6b445788479192e965fa23b490fb915
Japan Shah <64722088+japanshah17@users.noreply.github.com>
2021-07-14 14:47:26 +0530

    Add files via upload

--- MathsFun.java
+++ MathsFun.java
@@ -18,7 +18,7 @@ class MathsFun{
 		System.out.println("floor of " +f +" is " +Math.floor(f)); //returns double.. nearest higher int to argument
 		
 		c = 10;
-		System.out.println("log of " +c +" is " +Math.log(c));
+		System.out.println("log of " +c +" is " +Math.log(c)); //returns log to base e of argument
 		
 		a = 100;
 		b = 200;
@@ -37,6 +37,24 @@ class MathsFun{
 		
 		d = 4;
 		System.out.println("sqrt of " +d +" is " +Math.sqrt(d)); //returns double..squareroot of argument.. sqrt(144) = 12
+
+		d = 0;
+		System.out.println("tan of " +d +" is " +Math.tan(d)); //returns double.. tan of the argument
+
+		d = 6.28319;
+		System.out.println(d +" radian is " +Math.toDegrees(d) +" degrees"); //returns double..radian to degrees
+		
+		d = 360;
+		System.out.println(d +" degrees is " +Math.toRadians(d) +" radians"); //returns double..degrees to radian
+
+		d = 0;
+		System.out.println("asine of " +d +" is " +Math.asin(d)); //returns double.. asine of the argument
+		
+		d = 0;
+		System.out.println("acos of " +d +" is " +Math.acos(d)); //returns double.. acos of the argument
+		
+		d = 0;
+		System.out.println("atan of " +d +" is " +Math.atan(d)); //returns double.. atan of the argument
 
 		d = 5.3584631;
 		System.out.println("rint of " +d +" is " +Math.rint(d)); //returns double..closest integer to argument
